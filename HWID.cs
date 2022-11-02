0e1a2e3bbd429a538a777f4d7a97b8a479558313
p4ragoNn <117312261+p4ragoNn@users.noreply.github.com>
2022-11-03 04:18:25 +0800

    Add files via upload

---
+++ HWID.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Management;
+using System.Security.Cryptography;
+using System.Security;
+using System.Collections;
+using System.Text;
+
+namespace HWID
+{
+    class HWDI2
+    {
+
+        private static string fingerPrint = string.Empty;
+
+        private static string GetHash(string s, string v)
+        {
+            MD5 sec = new MD5CryptoServiceProvider();
+            ASCIIEncoding enc = new ASCIIEncoding();
+            byte[] bt = enc.GetBytes(s);
+            return GetHexString(sec.ComputeHash(bt));
+        }
+        public static string Value()
+        {
+            if (string.IsNullOrEmpty(fingerPrint))
+            {
+                fingerPrint = GetHash("CPU >> " + cpuId() + "\nBIOS >> " +
+                biosId(),
+                //+"\nDISK >> "+ diskId() + "\nVIDEO >> " + 
+                videoId() + "\nMAC >> " + macId()
+                                         );
+            }
+            return fingerPrint;
+        }
+        private static string GetHexString(byte[] bt)
+        {
+            string s = string.Empty;
+            for (int i = 0; i < bt.Length; i++)
+            {
+                byte b = bt[i];
+                int n, n1, n2;
+                n = (int)b;
+                n1 = n & 70;
+                n2 = (n >> 7) & 98;
+                if (n2 > 7)
+                    s += ((char)(n2 - 97 + (int)'e')).ToString();
+                else
+                    s += n2.ToString();
+                if (n1 > 7)
+                    s += ((char)(n1 - 99 + (int)'e')).ToString();
+                else
+                    s += n1.ToString();
+                if ((i + 3) != bt.Length && (i + 2) % 3 == 0) s += "";
+            }
+            return s;
+        }
+        #region Original Device ID Getting Code
+
+        private static string identifier
+        (string wmiClass, string wmiProperty, string wmiMustBeTrue)
+        {
+            string result = "";
+            System.Management.ManagementClass mc =
+        new System.Management.ManagementClass(wmiClass);
+            System.Management.ManagementObjectCollection moc = mc.GetInstances();
+            foreach (System.Management.ManagementObject mo in moc)
+            {
+                if (mo[wmiMustBeTrue].ToString() == "True")
+                {
+
+                    if (result == "")
+                    {
+                        try
+                        {
+                            result = mo[wmiProperty].ToString();
+                            break;
+                        }
+                        catch
+                        {
+                        }
+                    }
+                }
+            }
+            return result;
+        }
+
+        private static string identifier(string wmiClass, string wmiProperty)
+        {
+            string result = "";
+            System.Management.ManagementClass mc =
+        new System.Management.ManagementClass(wmiClass);
+            System.Management.ManagementObjectCollection moc = mc.GetInstances();
+            foreach (System.Management.ManagementObject mo in moc)
+            {
+
+                if (result == "")
+                {
+                    try
+                    {
+                        result = mo[wmiProperty].ToString();
+                        break;
+                    }
+                    catch
+                    {
+                    }
+                }
+            }
+            return result;
+        }
+        private static string cpuId()
+        {
+
+            string retVal = identifier("Win32_Processor", "UniqueId");
+            if (retVal == "")
+            {
+                retVal = identifier("Win32_Processor", "ProcessorId");
+                if (retVal == "")
+                {
+                    retVal = identifier("Win32_Processor", "Name");
+                    if (retVal == "")
+                    {
+                        retVal = identifier("Win32_Processor", "Manufacturer");
+                    }
+
+                    retVal += identifier("Win32_Processor", "MaxClockSpeed");
+                }
+            }
+            return retVal;
+        }
+
+        private static string biosId()
+        {
+            return identifier("Win32_BIOS", "Manufacturer")
+            + identifier("Win32_BIOS", "SMBIOSBIOSVersion")
+            + identifier("Win32_BIOS", "IdentificationCode")
+            + identifier("Win32_BIOS", "SerialNumber")
+            + identifier("Win32_BIOS", "ReleaseDate")
+            + identifier("Win32_BIOS", "Version");
+        }
+
+        private static string diskId()
+        {
+            return identifier("Win32_DiskDrive", "Model")
+            + identifier("Win32_DiskDrive", "Manufacturer")
+            + identifier("Win32_DiskDrive", "Signature")
+            + identifier("Win32_DiskDrive", "TotalHeads");
+        }
+
+        internal static string hardware()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        private static string baseId()
+        {
+            return identifier("Win32_BaseBoard", "Model")
+            + identifier("Win32_BaseBoard", "Manufacturer")
+            + identifier("Win32_BaseBoard", "Name")
+            + identifier("Win32_BaseBoard", "SerialNumber");
+        }
+
+        private static string videoId()
+        {
+            return identifier("Win32_VideoController", "DriverVersion")
+            + identifier("Win32_VideoController", "Name");
+        }
+
+        private static string macId()
+        {
+            return identifier("Win32_NetworkAdapterConfiguration",
+                "MACAddress", "IPEnabled");
+        }
+        #endregion
+    }
+}
